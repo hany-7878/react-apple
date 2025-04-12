f19bc0442f619a48e782885bf3f4ec3a9b2e1d80
hany-7878 <htesfaye641@gmail.com>
2025-04-12 12:14:48 -0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Replace with your repo name!
 export default defineConfig({
+  base: '/react-apple/',
   plugins: [react()],
-  // server: {
-  //   port: 3000, 
-   
-  // },
 });
